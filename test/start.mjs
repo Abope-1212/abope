d866c1033b6cda4f32d8df02e982eeda3c2fa8e7
unknown <waleagbeniga06@gmail.com>
2024-09-04 18:33:51 +0100

    get ready

---
+++ test/start.mjs
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+
+describe("Math Operations", function () {
+  it("should add numbers correctly", function () {
+    const num1 = 2;
+    const num2 = 3;
+    expect(num1 + num2).to.equal(5);
+  });
+  it("should not give a result 6", function () {
+    const num1 = 3;
+    const num2 = 3;
+    expect(num1 + num2).not.to.equal(6);
+  });
+});
